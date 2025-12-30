4f2cf2d6164db606f63275ac67455f104d69d862
Mr Unknown <168432129+vijitharanawakage@users.noreply.github.com>
2025-12-30 23:27:16 +0530

    Add files via upload

--- lib/WABinary/generic-utils.d.ts
+++ lib/WABinary/generic-utils.d.ts
@@ -1,17 +1,121 @@
-/// <reference types="node" />
-import { proto } from '../../WAProto';
-import { BinaryNode } from './types';
-export declare const getBinaryNodeChildren: (node: BinaryNode | undefined, childTag: string) => BinaryNode[];
-export declare const getAllBinaryNodeChildren: ({ content }: BinaryNode) => BinaryNode[];
-export declare const getBinaryNodeChild: (node: BinaryNode | undefined, childTag: string) => BinaryNode | undefined;
-export declare const getBinaryNodeChildBuffer: (node: BinaryNode | undefined, childTag: string) => Uint8Array | Buffer | undefined;
-export declare const getBinaryNodeChildString: (node: BinaryNode | undefined, childTag: string) => string | undefined;
-export declare const getBinaryNodeChildUInt: (node: BinaryNode, childTag: string, length: number) => number | undefined;
-export declare const assertNodeErrorFree: (node: BinaryNode) => void;
-export declare const reduceBinaryNodeToDictionary: (node: BinaryNode, tag: string) => {
-    [_: string]: string;
-};
-export declare const getBinaryNodeMessages: ({ content }: BinaryNode) => proto.WebMessageInfo[];
-export declare function binaryNodeToString(node: BinaryNode | BinaryNode['content'], i?: number): any;
-export declare const getBinaryNodeFilter: (node: BinaryNode[]) => boolean;
-export declare const getAdditionalNode: (name: string) => BinaryNode[];
+import { Boom } from '@hapi/boom'
+import { proto } from '../../WAProto'
+import { BinaryNode } from './types'
+
+// some extra useful utilities
+
+export const getBinaryNodeChildren = (node: BinaryNode | undefined, childTag: string) => {
+	if(Array.isArray(node?.content)) {
+		return node!.content.filter(item => item.tag === childTag)
+	}
+
+	return []
+}
+
+export const getAllBinaryNodeChildren = ({ content }: BinaryNode) => {
+	if(Array.isArray(content)) {
+		return content
+	}
+
+	return []
+}
+
+export const getBinaryNodeChild = (node: BinaryNode | undefined, childTag: string) => {
+	if(Array.isArray(node?.content)) {
+		return node?.content.find(item => item.tag === childTag)
+	}
+}
+
+export const getBinaryNodeChildBuffer = (node: BinaryNode | undefined, childTag: string) => {
+	const child = getBinaryNodeChild(node, childTag)?.content
+	if(Buffer.isBuffer(child) || child instanceof Uint8Array) {
+		return child
+	}
+}
+
+export const getBinaryNodeChildString = (node: BinaryNode | undefined, childTag: string) => {
+	const child = getBinaryNodeChild(node, childTag)?.content
+	if(Buffer.isBuffer(child) || child instanceof Uint8Array) {
+		return Buffer.from(child).toString('utf-8')
+	} else if(typeof child === 'string') {
+		return child
+	}
+}
+
+export const getBinaryNodeChildUInt = (node: BinaryNode, childTag: string, length: number) => {
+	const buff = getBinaryNodeChildBuffer(node, childTag)
+	if(buff) {
+		return bufferToUInt(buff, length)
+	}
+}
+
+export const assertNodeErrorFree = (node: BinaryNode) => {
+	const errNode = getBinaryNodeChild(node, 'error')
+	if(errNode) {
+		throw new Boom(errNode.attrs.text || 'Unknown error', { data: +errNode.attrs.code })
+	}
+}
+
+export const reduceBinaryNodeToDictionary = (node: BinaryNode, tag: string) => {
+	const nodes = getBinaryNodeChildren(node, tag)
+	const dict = nodes.reduce(
+		(dict, { attrs }) => {
+			dict[attrs.name || attrs.config_code] = attrs.value || attrs.config_value
+			return dict
+		}, { } as { [_: string]: string }
+	)
+	return dict
+}
+
+export const getBinaryNodeMessages = ({ content }: BinaryNode) => {
+	const msgs: proto.WebMessageInfo[] = []
+	if(Array.isArray(content)) {
+		for(const item of content) {
+			if(item.tag === 'message') {
+				msgs.push(proto.WebMessageInfo.decode(item.content as Buffer))
+			}
+		}
+	}
+
+	return msgs
+}
+
+function bufferToUInt(e: Uint8Array | Buffer, t: number) {
+	let a = 0
+	for(let i = 0; i < t; i++) {
+		a = 256 * a + e[i]
+	}
+
+	return a
+}
+
+const tabs = (n: number) => '\t'.repeat(n)
+
+export function binaryNodeToString(node: BinaryNode | BinaryNode['content'], i = 0) {
+	if(!node) {
+		return node
+	}
+
+	if(typeof node === 'string') {
+		return tabs(i) + node
+	}
+
+	if(node instanceof Uint8Array) {
+		return tabs(i) + Buffer.from(node).toString('hex')
+	}
+
+	if(Array.isArray(node)) {
+		return node.map((x) => tabs(i + 1) + binaryNodeToString(x, i + 1)).join('\n')
+	}
+
+	const children = binaryNodeToString(node.content, i + 1)
+
+	const tag = `<${node.tag} ${Object.entries(node.attrs || {})
+		.filter(([, v]) => v !== undefined)
+		.map(([k, v]) => `${k}='${v}'`)
+		.join(' ')}`
+
+	const content: string = children ? `>\n${children}\n${tabs(i)}</${node.tag}>` : '/>'
+
+	return tag + content
+}
