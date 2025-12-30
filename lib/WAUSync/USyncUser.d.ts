61b362976ea9d46d0027eafafd93fcf3fb230c1c
Mr Unknown <168432129+vijitharanawakage@users.noreply.github.com>
2025-12-30 23:24:43 +0530

    Add files via upload

--- lib/WAUSync/USyncUser.d.ts
+++ lib/WAUSync/USyncUser.d.ts
@@ -1,12 +1,27 @@
-export declare class USyncUser {
-    id: string;
-    lid: string;
-    phone: string;
-    type: string;
-    personaId: string;
-    withId(id: string): this;
-    withLid(lid: string): this;
-    withPhone(phone: string): this;
-    withType(type: string): this;
-    withPersonaId(personaId: string): this;
+export class USyncUser {
+	id: string
+	lid: string
+	phone: string
+	type: string
+
+	withId(id: string) {
+		this.id = id
+		return this
+	}
+
+	withLid(lid: string) {
+		this.lid = lid
+		return this
+	}
+
+	withPhone(phone: string) {
+		this.phone = phone
+		return this
+	}
+
+	withType(type: string) {
+		this.type = type
+		return this
+	}
+
 }
