18b55de420071331d8df99e1665e3fbdb830ea47
Mr Unknown <168432129+vijitharanawakage@users.noreply.github.com>
2025-12-30 23:10:38 +0530

    Add files via upload

--- lib/Socket/Client/abstract-socket-client.d.ts
+++ lib/Socket/Client/abstract-socket-client.d.ts
@@ -1,17 +1,19 @@
-/// <reference types="node" />
-/// <reference types="node" />
-import { EventEmitter } from 'events';
-import { URL } from 'url';
-import { SocketConfig } from '../../Types';
-export declare abstract class AbstractSocketClient extends EventEmitter {
-    url: URL;
-    config: SocketConfig;
-    abstract get isOpen(): boolean;
-    abstract get isClosed(): boolean;
-    abstract get isClosing(): boolean;
-    abstract get isConnecting(): boolean;
-    constructor(url: URL, config: SocketConfig);
-    abstract connect(): Promise<void>;
-    abstract close(): Promise<void>;
-    abstract send(str: Uint8Array | string, cb?: (err?: Error) => void): boolean;
+import { EventEmitter } from 'events'
+import { URL } from 'url'
+import { SocketConfig } from '../../Types'
+
+export abstract class AbstractSocketClient extends EventEmitter {
+	abstract get isOpen(): boolean
+	abstract get isClosed(): boolean
+	abstract get isClosing(): boolean
+	abstract get isConnecting(): boolean
+
+	constructor(public url: URL, public config: SocketConfig) {
+		super()
+		this.setMaxListeners(0)
+	}
+
+	abstract connect(): Promise<void>
+	abstract close(): Promise<void>
+	abstract send(str: Uint8Array | string, cb?: (err?: Error) => void): boolean;
 }
